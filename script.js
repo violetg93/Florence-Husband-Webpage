1a40d131f34f2e5d0f1c620029977429476dd85f
violetg93 <florence.husband93@gmail.com>
2026-06-08 17:31:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Taylor Swift Page</title>
+    
+    <style>
+        /* Your working pink headline code */
+        h2 {
+            color: rgb(223, 136, 151); 
+        }      
+    </style>
+</head>
+<body>
+
+    <h1>Taylor Swift</h1>
+    
+    <div style="text-align: center;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Taylor_Swift_Red_Tour_2%2C_2013.jpg/960px-Taylor_Swift_Red_Tour_2%2C_2013.jpg" alt="Taylor Swift Red Tour" width="400">
+    </div>
+
+    <p>Taylor Alison Swift was born on the 13th of December, 1989 in Pennsylvania. She is a leading cultural figure, and <strong> one of the best-selling music artists of all time. </strong></p>
+    
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Taylor_Swift_045_%2817682880264%29.jpg/960px-Taylor_Swift_045_%2817682880264%29.jpg" width="300">
+
+    <p> 
+        I love her because I relate to her songwriting, and because she sings beautifully. My favourite albums are 1989, reputation and folklore. My favourite song is False God from the album <em> Lover. </em> Most Taylor Swift fans agree that 1989 is her best album, but I think it's <em> reputation </em> because the songs are so catchy, well-written and intense. My favourite songs of all time by her are All Too Well, my tears ricochet, Style and False God.
+    </p>
+
+    <h2> First three albums </h2>
+    <ol type="1">
+        <li> Taylor Swift</li>
+        <li> Fearless </li>
+        <li> Speak Now</li> 
+    </ol>
+    
+    <h3> Instruments she plays</h3>
+    <ul style="list-style-type:disc;">
+        <li>guitar</li>
+        <li>piano</li>
+        <li>banjo</li>
+    </ul>
+
+    <p>Taylor performing live:</p>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Taylor_Swift_Reputation_Tour1_%28cropped%29.jpg/960px-Taylor_Swift_Reputation_Tour1_%28cropped%29.jpg" alt="Taylor Swift Reputation Tour" width="300">
+
+</body>
+</html>
